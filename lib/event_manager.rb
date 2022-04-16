f53f6cd6740650e41cdfc46db5ac33194178e983
mattand <mjande@umich.edu>
2022-04-16 13:53:54 -0400

    Add method for finding best days and hours

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,7 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'time'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
@@ -11,6 +12,20 @@ def clean_phone_numbers(phone_number)
   phone_number.delete_prefix!('1') if phone_number.length == 11
   phone_number = nil unless phone_number.length == 10
   phone_number
+end
+
+def find_best_hours(hours)
+  max_registration_hours = hours.values.max(3)
+  ideal_hours = hours.select { |_, value| value > max_registration_hours[2] }
+  ideal_hours.transform_keys! { |hour| "#{hour}:00" }
+  puts ideal_hours.keys
+end
+
+def find_best_days(days)
+  max_registration_days = days.values.max(3)
+  ideal_days = days.select { |_, value| value > max_registration_days[2] }
+  ideal_days.transform_keys! { |day| Date::DAYNAMES[day] }
+  puts ideal_days.keys
 end
 
 def legislators_by_zipcode(zip)
@@ -49,7 +64,9 @@ contents = CSV.open(
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
-=begin 
+hours = Hash.new(0)
+days = Hash.new(0)
+
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
@@ -58,10 +75,19 @@ contents.each do |row|
 
   legislators = legislators_by_zipcode(zipcode)
 
+  phone_numbers = clean_phone_numbers(row[:homephone])
+
+  time = row[:regdate]
+  t = Time.strptime(time, '%D %H')
+  hour = t.hour
+  weekday = t.wday
+  hours[hour] += 1
+  days[weekday] += 1
+
   form_letter = erb_template.result(binding)
 
   save_thank_you_letter(id, form_letter)
-end 
-=end
+end
 
-puts clean_phone_numbers('+1 (616) 780-1475')
+find_best_days(days)
+find_best_hours(hours)
